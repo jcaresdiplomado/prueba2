e618638b3dfdddddfd4442c364843939f75e0275
Jazmín <j.gormaz@duocuc.cl>
2025-04-20 20:43:42 -0400

    se agrega log al Controller

--- src/main/java/com/ejemplo/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/ejemplo/calculadora/controller/CalculadoraController.java
@@ -8,11 +8,14 @@ public class CalculadoraController {
 
     @GetMapping("/")
     public String mostrarCalculadora() {
+	System.out.println("mostrando calculadora");
         return "calculadora";
     }
 
     @GetMapping("/sumar")
     public double sumar(@RequestParam double a, @RequestParam double b) {
+	System.out.println("sumando " + a + " + " + b);
+
         return a + b;
     }
 
